3d211e77cc21684081e9c173f13ebd84c6ccef11
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-22 22:23:20 -0500

    Update equipos.vue

--- nuxt3/pages/equipos.vue
+++ nuxt3/pages/equipos.vue
@@ -62,6 +62,27 @@
         </v-col>
       </v-row>
     </v-container>
+            <v-container>
+            <v-row>
+                <v-col>
+                    <v-card @click="showModal = true">
+                        sdfsdf
+                    </v-card>
+                    <transition name="fade" appear>
+                        <div class="modal-overlay" v-if="showModal" @click="showModal = false"></div>
+                    </transition>
+                    <transition name="slide" appear>
+                        <div class="modal" v-if="showModal">
+                            <h1>Lorem Ipsum</h1>
+                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem provident explicabo accusamus laudantium voluptatum nobis sed nesciunt neque possimus molestiae?</p>
+                            <button class="button" @click="showModal = false">
+                                Close Modal
+                            </button>
+                        </div>
+                    </transition>
+                </v-col>
+            </v-row>
+        </v-container>
     <br />
     <!--pie de pagina -->
     <pie />
@@ -133,6 +154,7 @@ export default {
   data() {
     return {
       p1: imagen1,
+      showModal: false,
       cards: [
         {
           id: 1,
@@ -218,6 +240,67 @@ export default {
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+}
+</style>
+<style lang="scss" scoped>
+
+.modal-overlay {
+ position: absolute;
+ top: 0;
+ left: 0;
+ right: 0;
+ bottom: 0;
+ z-index: 98;
+ background-color: rgba(0, 0, 0, 0.3);
+}
+
+.modal {
+ position: fixed;
+ top: 50%;
+ left: 50%;
+ transform: translate(-50%, -50%);
+ z-index: 99;
+ 
+ width: 100%;
+ max-width: 400px;
+ background-color: #FFF;
+ border-radius: 16px;
+ 
+ padding: 25px;
+ 
+ h1 {
+  color: #222;
+  font-size: 32px;
+  font-weight: 900;
+  margin-bottom: 15px;
+ }
+ 
+ p {
+  color: #666;
+  font-size: 18px;
+  font-weight: 400;
+  margin-bottom: 15px;
+ }
+}
+
+.fade-enter-active,
+.fade-leave-active {
+ transition: opacity .5s;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+ opacity: 0;
+}
+
+.slide-enter-active,
+.slide-leave-active {
+ transition: transform .5s;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+ transform: translateY(-50%) translateX(100vw);
 }
 </style>
 

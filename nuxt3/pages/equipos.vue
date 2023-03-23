6513cbdf49c65bfa6bb9a0d3349cc51f5d3c61b6
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-22 20:56:16 -0500

    Update equipos.vue

--- nuxt3/pages/equipos.vue
+++ nuxt3/pages/equipos.vue
@@ -63,28 +63,6 @@
       </v-row>
     </v-container>
     <br />
-        <v-dialog
-      v-model="dialog"
-      width="auto"
-    >
-      <template v-slot:activator="{ props }">
-        <v-btn
-          color="primary"
-          v-bind="props"
-        >
-          Open Dialog
-        </v-btn>
-      </template>
-
-      <v-card>
-        <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </v-card-text>
-        <v-card-actions>
-          <v-btn color="primary" block @click="dialog = false">Close Dialog</v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
     <!--pie de pagina -->
     <pie />
   </div>
@@ -155,7 +133,6 @@ export default {
   data() {
     return {
       p1: imagen1,
-       dialog: false,
       cards: [
         {
           id: 1,

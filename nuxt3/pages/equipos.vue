b79670fb5c06250b32c07f4fbc1d5fb4b862cbf6
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-24 19:18:07 -0500

    Update equipos.vue

--- nuxt3/pages/equipos.vue
+++ nuxt3/pages/equipos.vue
@@ -83,22 +83,6 @@
                 </v-col>
             </v-row>
         </v-container>
-              <v-btn
-        class="ma-2"
-        color="primary"
-        @click="expand = !expand"
-      >
-        Expand Transition
-      </v-btn>
-
-      <v-expand-transition>
-        <v-card
-          v-show="expand"
-          height="100"
-          width="100"
-          class="mx-auto bg-secondary"
-        ></v-card>
-      </v-expand-transition>
     <br />
     <!--pie de pagina -->
     <pie />

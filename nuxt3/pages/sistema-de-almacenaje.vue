7fea80c911dc6264c747c2de6a7c646061d3a223
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-25 13:36:28 -0500

    Update sistema-de-almacenaje.vue

--- nuxt3/pages/sistema-de-almacenaje.vue
+++ nuxt3/pages/sistema-de-almacenaje.vue
@@ -86,8 +86,9 @@
                           </v-col>
                           <v-col cols="12" sm="6" md="6" style="height: 100%">
                             <div class="d-none d-sm-flex">
-                                <div style="width:100%; height: 100%; min-height: 400px">
-                                    <v-img :src="card.src" style="width:100%; height: 100%; min-height: 400px" cover></v-img>
+                                <br />
+                                <div style="width:100%; height: 100%; min-height: 800px">
+                                    <v-img :src="card.src" style="width:100%; height: 100%; min-height: 800px" cover></v-img>
                                 </div>
                             </div>
                             <div class="d-flex d-sm-none">

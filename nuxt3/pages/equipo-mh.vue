3f1f8e48a5f56ba3b8267596864f1340612680bf
diego048 <103468763+diego048@users.noreply.github.com>
2023-03-24 21:29:48 -0500

    Update equipo-mh.vue

--- nuxt3/pages/equipo-mh.vue
+++ nuxt3/pages/equipo-mh.vue
@@ -169,6 +169,13 @@ export default {
       ],
       })
     },
+      unmounted(){
+    document.body.style.position = 'static';
+        document.body.style.top = '';
+        document.body.style.width = '';
+        document.body.style.overflowY = '';
+        window.scrollTo(0, this.scrollPosition);
+  },
   data() {
     return {
       p1: ima1,

e82391c469ec9df5ddfc9105a643ddc911c4c2ae
diego048 <103468763+diego048@users.noreply.github.com>
2023-04-03 09:10:27 -0500

    Update diseño-almacen.vue

--- nuxt3/pages/diseño-almacen.vue
+++ nuxt3/pages/diseño-almacen.vue
@@ -1,5 +1,5 @@
 <template>
-    <div style="overflow-x:hidden" v-if="false">
+    <div style="overflow-x:hidden">
       <div>
         <!-- La cabecera con la info-->
         <div position="relative">

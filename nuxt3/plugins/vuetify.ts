a9dbce75f9f02caf960e4dc93bae7797ecfcae50
diego048 <103468763+diego048@users.noreply.github.com>
2023-04-08 00:14:58 -0500

    Update vuetify.ts

--- nuxt3/plugins/vuetify.ts
+++ nuxt3/plugins/vuetify.ts
@@ -1,10 +1,80 @@
 import {createVuetify} from 'vuetify'
-import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { VCard } from 'vuetify/components/'
+import { VTabs } from 'vuetify/components/'
+import { VTab } from 'vuetify/components/'
+import { VContainer } from 'vuetify/components/'
+import { VCardText } from 'vuetify/components/'
+import { VCardActions } from 'vuetify/components/'
+import { VWindow } from 'vuetify/components/'
+import { VWindowItem } from 'vuetify/components/'
+import { VRow } from 'vuetify/components/'
+import { VCol } from 'vuetify/components/'
+import { VImg } from 'vuetify/components/'
+import { VCarousel } from 'vuetify/components/'
+import { VCarouselItem } from 'vuetify/components/'
+import { VCardTitle } from 'vuetify/components/'
+import { VCardSubtitle } from 'vuetify/components/'
+import { VDivider } from 'vuetify/components/'
+import { VLayout } from 'vuetify/components/'
+import { VNavigationDrawer } from 'vuetify/components/'
+import { VList } from 'vuetify/components/'
+import { VListItem } from 'vuetify/components/'
+import { VAppBar } from 'vuetify/components/'
+import { VAppBarNavIcon } from 'vuetify/components/'
+import { VAppBarTitle } from 'vuetify/components/'
+import { VSpacer } from 'vuetify/components/'
+import { VMenu } from 'vuetify/components/'
+import { VBtn } from 'vuetify/components/'
+import { VMain } from 'vuetify/components/'
+import { VSheet } from 'vuetify/components/'
+import { VSlideGroup } from 'vuetify/components/'
+import { VSlideGroupItem } from 'vuetify/components/'
+import { VScaleTransition } from 'vuetify/components/'
+import { VAlert } from 'vuetify/components/'
+import { VTimeline } from 'vuetify/components/'
+import { VTimelineItem } from 'vuetify/components/'
+import { VTable } from 'vuetify/components/'
 
 export default defineNuxtPlugin(nuxtApp => {
     const vuetify = createVuetify({
-        components,
+        components: {
+            VCard,
+            VTabs,
+            VTab,
+            VContainer,
+            VCardText,
+            VCardActions,
+            VWindow,
+            VWindowItem,
+            VRow,
+            VCol,
+            VImg,
+            VCarousel,
+            VCarouselItem,
+            VCardTitle,
+            VCardSubtitle,
+            VDivider,
+            VLayout,
+            VNavigationDrawer,
+            VList,
+            VListItem,
+            VAppBar,
+            VAppBarNavIcon,
+            VAppBarTitle,
+            VSpacer,
+            VMenu,
+            VBtn,
+            VMain,
+            VSheet,
+            VSlideGroup,
+            VSlideGroupItem,
+            VScaleTransition,
+            VAlert,
+            VTimeline,
+            VTimelineItem,
+            VTable,
+        },
         directives
     })
     nuxtApp.vueApp.use(vuetify)

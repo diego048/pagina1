60041ef6b2f0f8275e48b551cf07d10a60f79a9f
diego048 <103468763+diego048@users.noreply.github.com>
2023-05-13 23:54:58 -0500

    Update api.py

--- python/api.py
+++ python/api.py
@@ -6,7 +6,7 @@ import smtplib
 
 
 app=Flask(__name__)
-CORS(app, origins=['http://localhost:8080', 'http://localhost:80','http://107.23.179.170', 'http://3.232.232.195:8080'])
+CORS(app, origins=['https://www.logicorperu.com', 'https://logicorperu.com/'])
 
 @app.after_request
 def after_request(response):
